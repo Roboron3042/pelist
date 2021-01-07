ec7d5bd1ab6c23037614c66d3426ac4c35bf6170
Roboron3042 <robertoms258@gmail.com>
2021-01-07 19:21:11 +0100

    Cambiamos de base

--- src/main/java/db/DatabaseHelper.java
+++ src/main/java/db/DatabaseHelper.java
@@ -1,17 +1,22 @@
-package bd;
+package db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.pelist.main.R;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
 
     // Table Name
     public static final String TABLE_NAME = "LISTAS";
 
     // Table columns
     public static final String _ID = "_id";
-    public static final String NOMBRE = "nombre";
+    public static final String SUBJECT = "subject";
+    public static final String DESC = "description";
 
     // Database Information
     static final String DB_NAME = "PELIST.DB";
@@ -21,19 +26,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Creating table query
     private static final String CREATE_TABLE = "create table " + TABLE_NAME + "(" + _ID
-            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NOMBRE + " TEXT NOT NULL);";
-    private static final String INSERT_VISTAS = "insert into " + TABLE_NAME + "(" + _ID + "," + NOMBRE + ")"
-        + "values ( 0, 'Vistas' );";
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + SUBJECT + " TEXT NOT NULL, " + DESC + " TEXT);";
 
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String watched= context.getString(R.string.watched_movies_list);
+        String watched_desc = context.getString(R.string.watched_movies_list_description);
+        String pending= context.getString(R.string.pending_movies_list);
+        String pending_desc = context.getString(R.string.pending_movies_list_description);
         db.execSQL(CREATE_TABLE);
-        db.execSQL(INSERT_VISTAS);
+        db.execSQL("insert into " + TABLE_NAME +
+                " values ('0', '" + watched + "' , '" + watched_desc + "');");
+        db.execSQL("insert into " + TABLE_NAME +
+                " values ('1', '" + pending + "' , '" + pending_desc + "');");
     }
 
     @Override
@@ -42,4 +53,3 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 }
-
